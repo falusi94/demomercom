b175b7b58f55f610af049dbe6ee371c5d03a7d26
falusi94 <falusi.david94@gmail.com>
2017-11-20 00:31:39 +0100

    Add character and default movement

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+	public GameObject target;
+	public float followSpeed = 2.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float deltaTime = Time.deltaTime;
+		float fspeed = Mathf.Min(1.0f, followSpeed * deltaTime);
+
+		Vector3 oldPositon = transform.position;
+		Vector3 newPositon = oldPositon;
+		newPositon.x = target.transform.position.x;		
+		newPositon = oldPositon + (newPositon - oldPositon) * fspeed;		
+		transform.position = newPositon;
+	}
+}
---
+++ Assets/Movement.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour {
+	public float moveSpeed = 5f;
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float deltaTime = Time.deltaTime;
+		if (Input.GetKey (KeyCode.Escape))	Application.Quit ();
+
+		Vector3 dv = new Vector3 (0,0,0);
+		bool left = Input.GetAxis ("Horizontal") < 0;
+		bool right = Input.GetAxis ("Horizontal") > 0;
+		//bool up = Input.GetAxis("Vertical") > 0;
+		//bool down = Input.GetAxis("Vertical") < 0;
+
+		if (right) dv += new Vector3(moveSpeed * deltaTime,0,0);
+		if (left) dv += new Vector3(-moveSpeed * deltaTime,0,0);
+		//if (up)	dv += new Vector3(0,movespeed * dt,0);
+		//if (down)dv += new Vector3(0,-movespeed * dt,0);
+		transform.position += dv;
+		if (dv.magnitude > 0f)
+			animator.SetInteger ("AnimationState", 1);
+		else
+			animator.SetInteger ("AnimationState", 0);
+
+		Vector3 s = transform.localScale;
+		if (dv.x < 0)
+			transform.localScale = new Vector3 (-Mathf.Abs (s.x), s.y, s.z);
+		else
+			transform.localScale = new Vector3 (Mathf.Abs (s.x), s.y, s.z);
+	}
+}
