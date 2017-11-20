78f46467378481c793a3204ccb57c777406eab99
falusi94 <falusi.david94@gmail.com>
2017-11-20 14:50:52 +0100

    Add default attack animation

---
+++ Assets/Attack.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack : MonoBehaviour {
+	private Animator animator;
+
+	// Use this for initialization
+	void Start () {
+		animator = this.GetComponentInChildren<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKey (KeyCode.Space)) {
+			animator.SetBool ("isAttacking", true);
+			animator.CrossFade ("Attack1H", 1);
+		}
+	}
+}
