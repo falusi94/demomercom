3f246e40168dd350352336d64209110cfc9a6cd7
falusi94 <falusi.david94@gmail.com>
2017-11-28 09:14:30 +0100

    Add flag at level end

--- Assets/Movement.cs
+++ Assets/Movement.cs
@@ -7,6 +7,7 @@ public class Movement : MonoBehaviour {
 	public float runSpeed = 10f;
 	public float xMin;
 	public float xMax;
+	public Transform flag;
 	private Animator animator;
 
 	// Use this for initialization
@@ -36,6 +37,13 @@ public class Movement : MonoBehaviour {
 		//if (down)dv += new Vector3(0,-movespeed * dt,0);
 		transform.position += dv;
 	
+		if (Mathf.Abs(transform.position.x - xMax) < 1f) {
+			gameObject.SetActive (false);
+			Vector3 pos = flag.position;
+			pos.y += 20;
+			flag.position = pos;
+		}
+
 		if (dv.magnitude > 0f) {
 			if (Input.GetKey (KeyCode.LeftShift) || Input.GetKey (KeyCode.RightShift))
 				animator.SetInteger ("AnimationState", 3);
